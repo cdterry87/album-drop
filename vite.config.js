e66e46c5d1ada686e71dce06a3210ca11e8e4a82
Chase Terry <chase.terry87@gmail.com>
2022-12-13 21:08:41 -0600

    Fixed WSL2 issues in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
+    // NOTE: server object here is used to fix an issue with WSL2 on Windows (https://github.com/laravel/vite-plugin/issues/49)
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        // usePolling fix for WSL2 (https://vitejs.dev/config/server-options.html#server-watch)
+        // watch: {
+        //     usePolling: true 
+        // }
+    },
     plugins: [
         laravel({
             input: [
@@ -12,6 +22,7 @@ export default defineConfig({
                 ...refreshPaths,
                 'app/Http/Livewire/**',
             ],
+
         }),
     ],
 });

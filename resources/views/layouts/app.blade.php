5c2b083ae643aac66e708a4046d4041b698fec23
Chase Terry <chase.terry87@gmail.com>
2023-01-02 22:53:34 -0600

    Added google analytics. Close #12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,22 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-6DT2KSTGEL"
+    ></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-6DT2KSTGEL');
+    </script>
+
     <meta charset="utf-8">
     <meta
         name="author"

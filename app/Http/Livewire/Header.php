1ec4a314f2af2bf0c55a1c6f008b63e9c3209d0e
Chase Terry <chase.terry87@gmail.com>
2023-02-05 19:02:49 -0600

    Finished feature to sync artists with spotify

--- app/Http/Livewire/Header.php
+++ app/Http/Livewire/Header.php
@@ -20,10 +20,59 @@ class Header extends Component
     {
         $user = auth()->user();
 
+        // Connect to Spotify API as the current user
         $api = $this->createSpotifySession($user);
 
-        $userFollowedArtists = $api->getUserFollowedArtists();
+        // Sync artists with Spotify
+        $syncedToSpotify = $this->syncArtistsToSpotify($api, $user);
+        $syncedFromSpotify = $this->syncArtistsFromSpotify($api, $user);
 
+        // If we synced anything, redirect with a success message
+        if ($syncedToSpotify || $syncedFromSpotify) {
+            return redirect()
+                ->route('tracked-artists')
+                ->with('sync-message', 'Your artists have been successfully synced with Spotify!');
+        }
+
+        // Otherwise redirect with a warning message
+        return redirect()
+            ->route('tracked-artists')
+            ->with('no-sync-message', 'No artists were synced with Spotify. Track artists or follow more artists on Spotify to sync them.');
+    }
+
+    protected function syncArtistsToSpotify($api, $user)
+    {
+        // Get user's tracked artists from database
+        $trackedArtists = UserArtist::with('artist')->where('user_id', $user->id)->get();
+        $trackedArtistsIds = [];
+        $trackedArtistsCount = 0;
+        foreach ($trackedArtists as $artist) {
+            $trackedArtistsIds[] = $artist->artist->spotify_artist_id;
+
+            $trackedArtistsCount++;
+
+            // If we have 50 artists, we need to make a request to Spotify to follow them
+            if (count($trackedArtistsIds) == 50) {
+                $api->followArtistsOrUsers('artist', $trackedArtistsIds);
+                $trackedArtistsIds = [];
+            }
+        }
+
+        // If we have any artists left, make a request to Spotify to follow them
+        if (count($trackedArtistsIds) > 0) {
+            $api->followArtistsOrUsers('artist', $trackedArtistsIds);
+            $trackedArtistsIds = [];
+        }
+
+        if ($trackedArtistsCount > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    protected function syncArtistsFromSpotify($api, $user)
+    {
+        $userFollowedArtists = $api->getUserFollowedArtists();
         if ($userFollowedArtists) {
             $artists = $userFollowedArtists->artists->items;
 
@@ -44,13 +93,8 @@ class Header extends Component
                 ]);
             }
 
-            return redirect()
-                ->route('tracked-artists')
-                ->with('sync-message', 'Your followed artists were synced successfully from Spotify.');
+            return true;
         }
-
-        return redirect()
-            ->route('tracked-artists')
-            ->with('no-sync-message', 'No followed artists wwere found from your Spotify account.');
+        return false;
     }
 }
